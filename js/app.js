cd52ebdaaff5917b5d7eea32767ef71a742acaf3
Marcus Gonzales <codeforlife86@outlook.com>
2018-09-05 11:02:20 -0700

    Updated app.js with Basic Enemy & Player

--- js/app.js
+++ js/app.js
@@ -1,19 +1,27 @@
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+// Golbal Consts
+const TILEHEIGHT = 70;
+const TILEWIDTH = 100;
 
+// Enemies our player must avoid
+var Enemy = function(y) {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
+    this.y = y;
+    this.x = 0;
     this.sprite = 'images/enemy-bug.png';
+    this.random = Math.floor(Math.random() * 3) + 1;
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+    // dt = time delta between ticks
+    this.x = Math.round(this.x + (this.random + 1 * dt));
+
+    if (this.x > 600) {
+      this.x = 0;
+      this.random = Math.floor(Math.random() * 3) + 1;
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,12 +32,26 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+let Player = function() {
+  // Starting pos
+  this.x = 200;
+  this.y = 350;
+  this.sprite = 'images/char-boy.png';
+};
 
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.update = function() {
+
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+let allEnemies = [new Enemy(70), new Enemy(140), new Enemy(210)];
+let player = new Player();
 
 
 // This listens for key presses and sends the keys to your
