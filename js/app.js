d1fa05d8fbeed72c922fdf7b97f93d7b877f79a2
Marcus Gonzales <codeforlife86@outlook.com>
2018-09-05 16:30:10 -0700

    Updated app.js with Player Movement

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // Golbal Consts
-const TILEHEIGHT = 70;
+const TILEHEIGHT = 83;
 const TILEWIDTH = 100;
 
 // Enemies our player must avoid
@@ -13,7 +13,6 @@ var Enemy = function(y) {
 };
 
 // Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
     // dt = time delta between ticks
     this.x = Math.round(this.x + (this.random + 1 * dt));
@@ -29,9 +28,7 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+// Player Class
 let Player = function() {
   // Starting pos
   this.x = 200;
@@ -39,12 +36,30 @@ let Player = function() {
   this.sprite = 'images/char-boy.png';
 };
 
+// a render() method
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+// a update() method
 Player.prototype.update = function() {
 
+};
+
+// a handleInput() method.
+Player.prototype.handleInput = function(kc) {
+  if(kc === 'left' && this.x > 0) {
+    this.x = this.x - TILEWIDTH;
+  }
+  if(kc === 'right' && this.x < 400) {
+    this.x = this.x + TILEWIDTH;
+  }
+  if(kc === 'up' && this.y > 0) {
+    this.y = this.y - TILEHEIGHT;
+  }
+  if(kc === 'down' && this.y < 400) {
+    this.y = this.y + TILEHEIGHT;
+  }
 };
 
 // Now instantiate your objects.
@@ -56,12 +71,18 @@ let player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
+
+// Added keys for WASD movement
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
         38: 'up',
         39: 'right',
-        40: 'down'
+        40: 'down',
+        65: 'left', // a
+        87: 'up',   // w
+        68: 'right',// d
+        83: 'down'  // s
     };
 
     player.handleInput(allowedKeys[e.keyCode]);
